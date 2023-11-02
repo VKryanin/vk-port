8c09feacc7266c67c64c6c46d292ec145740865e
Vitaly Kryanin <krianinvv@internet.ru>
2023-11-02 14:59:48 +0300

    fix bug with burder menu

--- src/components/Burger/Burger.jsx
+++ src/components/Burger/Burger.jsx
@@ -1,24 +1,22 @@
-import { useState } from 'react';
+import { useState, useRef, forwardRef } from 'react';
 import styles from './Burger.module.scss';
 
 import { BurgerMenu } from '../BurgerMenu/BurgerMenu';
 
 export const Burger = () => {
     const [Open, setOpen] = useState(false)
-
+    const refBurgerMenu = useRef(null);
     const handleClick = () => {
         setOpen(!Open)
-        console.log(Open);
     }
 
-
     return (
-        <div className={styles.burger}>
+        <div className={styles.burger} >
             <div
                 className={!Open ? styles.burgerArray : `${styles.burgerArray} ${styles.burgerCross}`}
                 onClick={handleClick}
             />
-            {<BurgerMenu status={Open} />}
+            {<BurgerMenu  status={Open} setOpen={setOpen} />}
         </div>
 
     )
--- src/components/BurgerMenu/BurgerMenu.jsx
+++ src/components/BurgerMenu/BurgerMenu.jsx
@@ -1,51 +1,9 @@
+import React from 'react';
 import { Link } from "react-scroll";
-import React, { useState, useEffect, useLayoutEffect } from 'react';
 
 import styles from './BurgerMenu.module.scss';
 
 export const BurgerMenu = ({ status }) => {
-    const [activeLink, setActiveLink] = useState('');
-    const [scrollPosition, setScrollPosition] = useState(0);
-    const handleScroll = () => {
-        const position = window.pageYOffset;
-        setScrollPosition(position);
-    };
-    useEffect(() => {
-        window.addEventListener('scroll', handleScroll, { passive: true });
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-
-    useLayoutEffect(() => {
-        const aboutSection = document.getElementById('about');
-        const educationSection = document.getElementById('education');
-        const experienceSection = document.getElementById('experience');
-        const projectsSection = document.getElementById('projects');
-
-        const position = scrollPosition + window.innerHeight / 2;
-
-        if (
-            position >= aboutSection.offsetTop &&
-            position < educationSection.offsetTop
-        ) {
-            setActiveLink('about');
-        } else if (
-            position >= educationSection.offsetTop &&
-            position < experienceSection.offsetTop
-        ) {
-            setActiveLink('education');
-        } else if (
-            position >= experienceSection.offsetTop &&
-            position < projectsSection.offsetTop
-        ) {
-            setActiveLink('experience');
-        } else if (position >= projectsSection.offsetTop) {
-            setActiveLink('projects');
-        } else {
-            setActiveLink('');
-        }
-    }, [scrollPosition]);
 
     return (
         <nav className={status ? `${styles.burger} ${styles.burgerIsShow}` : styles.burger}>
@@ -55,8 +13,7 @@ export const BurgerMenu = ({ status }) => {
                         to='about'
                         smooth={true}
                         duration={500}
-                        className={`${styles.burgerLink} ${activeLink === 'about' ? styles.burgerLinkActive : ''}`}
-                        activeClass={styles.burgerLinkActive}
+                        className={styles.burgerLink}
                     >
                         About
                     </Link>
@@ -66,8 +23,7 @@ export const BurgerMenu = ({ status }) => {
                         to='education'
                         smooth={true}
                         duration={500}
-                        className={`${styles.burgerLink} ${activeLink === 'education' ? styles.burgerLinkActive : ''}`}
-                        activeClass={styles.burgerLinkActive}
+                        className={styles.burgerLink}
                     >
                         Education
                     </Link>
@@ -77,8 +33,7 @@ export const BurgerMenu = ({ status }) => {
                         to='experience'
                         smooth={true}
                         duration={500}
-                        className={`${styles.burgerLink} ${activeLink === 'experience' ? styles.burgerLinkActive : ''}`}
-                        activeClass={styles.burgerLinkActive}
+                        className={styles.burgerLink}
                     >
                         Experience
                     </Link>
@@ -88,8 +43,7 @@ export const BurgerMenu = ({ status }) => {
                         to='projects'
                         smooth={true}
                         duration={500}
-                        className={`${styles.burgerLink} ${activeLink === 'projects' ? styles.burgerLinkActive : ''}`}
-                        activeClass={styles.burgerLinkActive}
+                        className={styles.burgerLink}
                     >
                         All projects
                     </Link>
