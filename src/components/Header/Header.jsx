2f0b315276b2ee35053859814f0c88c95dce013b
Vitaly Kryanin <krianinvv@internet.ru>
2024-02-13 20:21:37 +0300

    start adaptive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,15 +4,19 @@ import styles from './Header.module.scss';
 import photo from '../../images/img_normal.png'
 
 import { Logo } from '../Logo/Logo';
+import { useState } from 'react';
+
 
 export const Header = () => {
+    const [isOpen, setIsOpne] = useState(false)
+
     return (
         <header className={styles.header}>
             <Logo />
             <div className={styles.headerWrapperImage}>
                 <img className={styles.headerImage} src={photo} alt="me" />
             </div>
-
+            <div className={`${styles.headerBurger} ${isOpen ? styles.headerBurgerActive : ''}`} onClick={()=> setIsOpne(!isOpen)} />
         </header>
     )
 }
