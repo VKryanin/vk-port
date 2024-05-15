f30b955b480da0aadbbcbde9b656ef3316e22cd2
Vitaly Kryanin <krianinvv@internet.ru>
2024-05-15 19:46:52 +0300

    isdone

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import Typewriter from 'typewriter-effect';
 
 import styles from './About.module.scss';
 
@@ -7,31 +8,40 @@ export const About = ({ id }) => {
 
     return (
         <div id={id} className={styles.about}>
-            <h3 className={styles.aboutTitle}>
-                <span className={styles.aboutGreen}>
-                    Bringing</span>&nbsp;ideas
-            </h3>
-            <h4 className={styles.aboutTitle}>
-                to&nbsp;
-                <span className={styles.aboutBlack}>life</span>
-                &nbsp;with&nbsp;
-                <span className={styles.aboutGrey}>code</span>
-            </h4>
+            <Typewriter
+                className={styles.aboutTypewriter}
+                options={{
+                    cursor: '_',
+                    loop: false,
+                    cursorClassName: `${styles.aboutCursor}`
+                }}
+                onInit={(typewriter) => {
+                    typewriter
+                        .typeString(`<span class=${styles.aboutGreen}>Htfkbpe. </span> <span class=${styles.aboutTitle}> bltb</span>`)
+                        .pauseFor(500)
+                        .deleteAll()
+                        .typeString(`<span class=${styles.aboutGreen}>Реализую</span><span class=${styles.aboutTitle}> идеи</span>`)
+                        // .pauseFor(1000)
+                        .typeString(`<span class=${styles.aboutTitle}> в <span class=${styles.aboutBlack}>жизнь</span> с&nbsp;помощью <span class=${styles.aboutGrey} > кода</span></span>`)
+                        .start();
+                }}
+            />
+
             <p className={styles.aboutText}>
-                Свой путь в програмирование я начал с изучения Java, Python и работой тестировщика.
-                Первым моим проектом был <Link className={styles.aboutLink} to="http://cocshop.ru/" target='_blank'>интернет магазин</Link> для друга.
-                Этот небольшой опыт показал мне, что разработка меня увлекает полностью и я принял решение пройти обучение от
+                Мой путь в программировании начался с изучения Java и Python, а также с работы на позиции тестировщика.
+                Мой первый проект был <Link className={styles.aboutLink} to="http://cocshop.ru/" target='_blank'>интернет-магазин</Link> для друга.
+                Этот небольшой опыт показал мне, что разработка полностью меня увлекает, и я принял решение пройти обучение на курсах от
                 <Link className={styles.aboutLink} to="https://practicum.yandex.ru/frontend-developer/?from=catalog" target='_blank'> Яндекс.Практикум</Link> в 2022.
-                В 2023 году я успешно сдал дипломную работу по направлению&nbsp;"Веб&nbsp;разработчик".
-                Во время обучения, я устроился на проектую работу в стартап <Link className={styles.aboutLink} to="https://tessera.hopto.org/" target='_blank'> Roman Mosaic School </Link>
-                и параллельно начал обучение на платформе <Link className={styles.aboutLink} to="https://app.rs.school/" target='_blank'>RollingScopes</Link>, созданой frontend разработчиками.
+                В 2023 году я успешно защитил дипломную работу по направлению&nbsp;"Веб-&nbsp;разработчик".
+                Во время обучения я также устроился на проектную работу в стартапе <Link className={styles.aboutLink} to="https://tessera.hopto.org/" target='_blank'> Roman Mosaic School </Link>
+                и параллельно начал изучение на платформе <Link className={styles.aboutLink} to="https://app.rs.school/" target='_blank'>RollingScopes</Link>, созданной frontend разработчиками.
             </p>
             <p className={styles.aboutText}>
-                Моя цель — улучшить свои навыки и реализовать приобретенные навыки.
+                Моя цель - улучшить свои навыки и применить приобретенные знания в пользу компании.
             </p>
             <p className={styles.aboutText}>
-                По мимо программирования, я хожу в зал, играю в настольные игры, выезжаю с палатками
-                или смотрю сериалы, фильмы.
+                Когда я не занят программированием, я предпочитаю проводить время за настольными играми с друзьями, ходить в походы или поглащать контент фильмов, сериалов.
+                Стараюсь регулярно ходить в спортзал, чтобы оставаться в форме и поддерживать энергию на день.
             </p>
         </div >
     )
--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -43,8 +43,8 @@ export const Experience = ({ id }) => {
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg>
                             </h6>
                             <p className={styles.experienceSubtitle}>Веб-разработчик</p>
-                            <p className={styles.experienceText}>Разработка внутренней CRM системы. Добавление новых нодулей, под запросы компании, рефакторинг прошлых модулей,
-                                исправление ошибок, добаработка функционала.</p>
+                            <p className={styles.experienceText}>Разработка внутренней CRM системы. Добавление новых модулей под запросы компании, рефакторинг прошлых модулей,
+                                исправление ошибок, доработка функционала.</p>
                         </div>
                     </Link>
                 </li>
@@ -61,8 +61,8 @@ export const Experience = ({ id }) => {
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg>
                             </h6>
                             <p className={styles.experienceSubtitle}>Frontend developer</p>
-                            <p className={styles.experienceText}>Проектная занятость, в старпапе онлайн и офлайн курсов по "Римской мозайке" в ходе работы разработал несколько модулей.
-                                Исправление ошибок, взаимодействие с дизайнерами.</p>
+                            <p className={styles.experienceText}>Проектная занятость в стартапе онлайн и офлайн курсов по "Римской мозайке" разработал несколько модулей.
+                            </p>
                         </div>
                     </Link>
                 </li>
