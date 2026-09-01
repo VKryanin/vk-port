080faed36116830f456811e6217a30e17b32517d
vitaly <v.kryanin@alkor-tech.ru>
2026-09-01 13:55:58 +0300

    Update portfolio descriptions

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -43,14 +43,14 @@ export const Experience = ({ id }) => {
                         className={`${styles.experienceItem} ${hoveredItem !== 0 && isHover ? styles.darken : ''}`}
                         onMouseEnter={() => handleItemHover(0)}
                         onMouseLeave={handleItemLeave}>
-                        <p className={styles.experienceData}>11.2023 - по настоящее время </p>
+                        <p className={styles.experienceData}>11.2023 - 10.2024 </p>
                         <div className={styles.experienceWrapper}>
                             <h6 className={styles.experienceLink}>I-Tech
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg>
                             </h6>
-                            <p className={styles.experienceSubtitle}>Веб-разработчик</p>
-                            <p className={styles.experienceText}>Разработка внутренней CRM системы. Добавление новых модулей под запросы компании, рефакторинг прошлых модулей,
-                                исправление ошибок, доработка функционала.</p>
+                            <p className={styles.experienceSubtitle}>Fullstack-разработчик</p>
+                            <p className={styles.experienceText}>Участвовал в продуктовой разработке CRM-системы, которая покрывала ключевые бизнес-процессы: контроль работы инженеров,
+                                отслеживание выполнения заказов и внедрение внутреннего электронного документооборота с дальнейшим масштабированием для оптимизации и автоматизации процессов.</p>
                         </div>
                     </Link>
                 </li>
@@ -67,7 +67,8 @@ export const Experience = ({ id }) => {
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="inline-block h-4 w-4 shrink-0 transition-transform group-hover/link:-translate-y-1 group-hover/link:translate-x-1 group-focus-visible/link:-translate-y-1 group-focus-visible/link:translate-x-1 motion-reduce:transition-none ml-1 translate-y-px" aria-hidden="true"><path fillRule="evenodd" d="M5.22 14.78a.75.75 0 001.06 0l7.22-7.22v5.69a.75.75 0 001.5 0v-7.5a.75.75 0 00-.75-.75h-7.5a.75.75 0 000 1.5h5.69l-7.22 7.22a.75.75 0 000 1.06z" clipRule="evenodd"></path></svg>
                             </h6>
                             <p className={styles.experienceSubtitle}>Frontend developer</p>
-                            <p className={styles.experienceText}>Проектная занятость в стартапе онлайн и офлайн курсов по "Римской мозайке" разработал несколько модулей.
+                            <p className={styles.experienceText}>Стартап по разработке сайта для курсов римской мозаики с возможностями интернет-магазина: добавлением товаров в корзину
+                                и регистрацией на онлайн- и офлайн-курсы.
                             </p>
                         </div>
                     </Link>
